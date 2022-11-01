565bba6c71a75032fb3dabe7428d30d9a7be516f
Derlidio Siqueira <derlidio@users.noreply.github.com>
2022-10-31 22:59:04 -0300

    Initial Commit

---
+++ EZTemplate.iOS/Main.cs
@@ -0,0 +1,27 @@
+﻿/*__ ____  _             
+| __|_  / /_\  _ __ _ __ 
+| _| / / / _ \| '_ \ '_ \
+|___/___/_/ \_\ .__/ .__/
+|  \/  |__ _| |_|__|_| _ 
+| |\/| / _` | / / -_) '_|
+|_|  |_\__,_|_\_\___|_|
+ 
+(C)2022 Derlidio Siqueira - Expoente Zero */
+
+using System;
+
+using UIKit;
+
+namespace EZTemplate.iOS
+{
+    public class Application
+    {
+        static void Main(string[] args)
+        {
+            Type principalClass = null;
+            Type appDelegate = typeof(AppDelegate);
+
+            UIApplication.Main(args, principalClass, appDelegate);
+        }
+    }
+}
